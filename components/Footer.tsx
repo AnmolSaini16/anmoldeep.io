802660976573ceb1ca3ec86e3f936000b99bc27d
AnmolSaini16 <sainianmol16@gmail.com>
2024-05-25 15:57:17 +0530

    Updated Copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="text-gray-500 h-8 text-center">
       <small className="block text-xs">
-        &copy; {new Date().getFullYear()} Anmol. All rights reserved.
+        &copy; 2024 Anmol. All rights reserved.
       </small>
     </footer>
   );
