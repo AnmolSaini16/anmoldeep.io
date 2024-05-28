f3e5c5335ce96dc8c1bfe1fd50c68f21a1bcca72
AnmolSaini16 <sainianmol16@gmail.com>
2024-05-28 12:35:29 +0530

    Blog page enhacement: Added views

--- app/blog/BlogPost.tsx
+++ app/blog/BlogPost.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Eye } from "lucide-react";
+import { Suspense } from "react";
 
 import { formatDate } from "@/lib/utils";
 import { IPost } from "@/types";
-import { Separator } from "@/components/ui/separator";
+import { getPostVIews } from "@/lib/action";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function BlogPost({
   post,
@@ -12,22 +15,27 @@ export default function BlogPost({
   post: IPost;
   index: number;
 }) {
+  const postViewsPromise = getPostVIews(post.id);
   return (
     <>
       <article className="group relative flex items-start justify-between gap-8">
-        <div className="space-y-1">
-          <h2 className="font-semibold md:text-lg">{post.title}</h2>
+        <div className="space-y-0.5 min-w-0">
+          <h2 className="font-semibold">{post.title}</h2>
           {post.description && (
-            <p className="text-muted-foreground leading-snug">
+            <p className="text-muted-foreground leading-snug truncate">
               {post.description}
             </p>
           )}
-
-          {post.published_timestamp && (
-            <p className="text-sm text-muted-foreground pt-1.5">
-              {formatDate(post.published_timestamp)}
-            </p>
-          )}
+          <div className="flex items-center justify-between pt-1">
+            {post.published_timestamp && (
+              <p className="text-sm text-muted-foreground">
+                {formatDate(post.published_timestamp)}
+              </p>
+            )}
+            <Suspense fallback={<Skeleton className="w-10 h-5" />}>
+              <PostViews postViewsPromise={postViewsPromise} />
+            </Suspense>
+          </div>
         </div>
 
         {post?.cover_image && (
@@ -47,7 +55,25 @@ export default function BlogPost({
           <span className="sr-only">View Article</span>
         </Link>
       </article>
-      <Separator className="last:hidden" />
     </>
   );
 }
+
+const PostViews = async ({
+  postViewsPromise,
+}: {
+  postViewsPromise: Promise<number | undefined>;
+}) => {
+  const postViews = await postViewsPromise;
+
+  return (
+    <>
+      {postViews !== undefined && (
+        <p className="text-sm text-muted-foreground flex items-center gap-0.5">
+          <Eye className="w-4 h-4" />
+          {postViews?.toLocaleString()}
+        </p>
+      )}
+    </>
+  );
+};
