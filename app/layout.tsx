c851025d5ddf93e2822fbb62f0c4b2401b791175
AnmolSaini16 <sainianmol16@gmail.com>
2024-11-08 12:25:03 +0530

    Updated og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
     title: siteConfig.name,
     description: siteConfig.description,
     siteName: siteConfig.name,
-    images: [`${siteConfig.url}/og.png`],
+    images: [`${siteConfig.url}/og.jpeg`],
   },
   icons: {
     icon: "/icon?<generated>",
